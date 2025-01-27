b0cbca604c60465f54cb56986ae7cff1660bddd5
Arsh <arshtopia@gmail.com>
2025-01-27 14:08:36 +0530

    added tos and privacy policy

--- src/app/components/Start.tsx
+++ src/app/components/Start.tsx
@@ -1,16 +1,33 @@
+import Link from "next/link";
+
 type TStartProps = {
     onClickStart: () => void;
 };
 
 export default function Start({ onClickStart }: TStartProps) {
     return (
-        <div className="my-auto min-h-screen min-w-screen flex flex-col items-center pt-32 gap-16">
-            <p className="prose-md whitespace-normal px-16 text-center">
+        <div className="my-auto min-h-screen min-w-screen flex flex-col items-center mt-16 gap-12 px-16">
+            <p className="prose-md whitespace-normal text-center">
                 This quiz will determine how much gay you are!
             </p>
             <button className="btn btn-primary mt-4" onClick={onClickStart}>
                 Start Quiz
             </button>
+            <p className="prose-sm text-gray-400 whitespace-normal text-center">
+                Disclaimer: This is just a test made for fun and does not determine your actual
+                sexuality. Do not bully others based on the results. Have fun!
+            </p>
+            <p className="prose-sm text-gray-400 whitespace-normal text-center">
+                I do not collect any data. However, I encourage you to checkout{" "}
+                <Link
+                    className="underline"
+                    target="blank"
+                    href="https://vercel.com/legal/privacy-policy"
+                >
+                    {"Vercel's Privacy Policy Here"}
+                </Link>
+                .
+            </p>
         </div>
     );
 }
